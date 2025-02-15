62d522ad8b187cbbf2e2c9e5b41bd2dc12475c27
Dean <designersuite@outlook.com>
2025-02-15 20:31:33 +0000

    Registered Table and Booking models in Django Admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Table, Booking  # Import the models
 
-# Register your models here.
+# Register Table model
+admin.site.register(Table)
+
+# Register Booking model
+admin.site.register(Booking)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,39 @@
+# Import Django's built-in models
 from django.db import models
 
-# Create your models here.
+# Import Django's built-in User model (used for authentication)
+from django.contrib.auth.models import User
+
+
+# =======================================
+# Table Model (Represents restaurant tables)
+# =======================================
+class Table(models.Model):
+    number = models.IntegerField(unique=True)  # Each table has a unique number
+    capacity = models.IntegerField()  # Number of seats available at each table
+
+    def __str__(self):
+        return f"Table {self.number} (Seats {self.capacity})"
+
+# ========================================
+# Booking Model (Represents reservaations)
+# ========================================
+
+
+class Booking(models.Model):
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )   # Links a booking to a user. If the user is deleted the booking is too
+    table = models.ForeignKey(
+        Table, on_delete=models.CASCADE
+    )   # Links a table to a user. If the table is deleted, its bookings also
+    date = models.DateField()   # The date of the reservation
+    time = models.TimeField()   # The time of the reservation
+    guests = models.IntegerField()   # Number of guests that are included
+
+    def __str__(self):
+        # Displays booking info in specific format below
+        return (
+            f"Booking by {self.customer.username} "
+            f"for {self.guests} guests on {self.date} at {self.time}"
+        )
