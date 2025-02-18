5532d377c8416d96e86b05e26f4a45d76e4b2203
Dean <designersuite@outlook.com>
2025-02-18 02:54:51 +0000

    Added working login page and updated URLs

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+def home(request):
+    return render(request, "home.html")
