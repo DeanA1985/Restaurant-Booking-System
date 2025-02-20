1f37de450425942dbbf33a2b1554fe9879d2e1ca
Dean <designersuite@outlook.com>
2025-02-20 21:57:30 +0000

    Fixed line length issues in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,19 @@ from . import views
 urlpatterns = [
     path(
         'login/',
-        auth_views.LoginView.as_view(template_name='registration/login.html'),
+        auth_views.LoginView.as_view(
+            template_name='registration/login.html'
+        ),
         name='login'
     ),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register'),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='home'),
+        name='logout'
+    ),
+    path(
+        'register/',
+        views.register,
+        name='register'
+    ),
 ]
